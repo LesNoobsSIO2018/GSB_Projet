c60b71ec5850e62d46d73a8817330b4f6ee79740
Ivan Naluzhnyi <Ivan Naluzhnyi@PC-NALUZHNYI>
2017-10-19 23:58:02 +0200

    Affichage de composants d'un medicament presque fait sauf il y a des probleme avec URL

--- application/views/v_CompsMeds.php
+++ application/views/v_CompsMeds.php
@@ -1,5 +1,25 @@
-<?php
 
-foreach ($lesComposantDeMedicaments as $comps){
-    var_demp($comps);
-}
+<table class='form-control'>
+                <tr>
+<!--                    <th class="thLC">
+                        Code d'un composant
+                    </th>-->
+                    <th class="thLC">
+                        Libelle d'un composant 
+                    </th>                 
+                </tr>
+     <?php
+       foreach ($lesComposantDeMedicaments as $comps){
+        ?>  
+                 <tr>
+<!--                    <td class="cLC">
+                          <?php echo $comps->CMP_CODE; ?>
+                    </td>-->
+                    <td class="cLC">
+                         <?php echo $comps->CMP_LIBELLE; ?>
+                    </td>
+                </tr>      
+     <?php           
+    }
+    ?>
+</table>
