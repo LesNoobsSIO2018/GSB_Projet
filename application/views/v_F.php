bd5ba656a6df694176d917959fe8fdcba0f2fc06
Ivan Naluzhnyi <Ivan Naluzhnyi@PC-NALUZHNYI>
2017-10-23 23:23:56 +0200

    Dernier modif

--- application/views/v_F.php
+++ application/views/v_F.php
@@ -1,4 +1,31 @@
+<html>
+    <head>
+        <title>GSB</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+               
+                    <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<h1>TEST</h1>
-<a href="<?php echo base_url(); ?>index.php/Ctrl_Medoc/index">Thomas</a>
-<a href="<?php echo base_url(); ?>index.php/Ctrl_Visiteur/index">Sandra</a>
+            <!-- Optional theme -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+            <style>
+                h{
+                   
+                }
+            </style>
+    </head>
+    <body>
+        <header>
+            <h1 class="text-left">GSB</h1>
+        </header>
+        <div class="container">
+            <a href="<?php echo base_url(); ?>index.php/Ctrl_Medoc/index">Thomas</a><br>
+            <a href="<?php echo base_url(); ?>index.php/Ctrl_Visiteur/index">Sandra</a><br>
+        </div>
+    </body>
+</html>
