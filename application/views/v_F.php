e8cfb110b5720a50a7f99694e7b3af2863b288df
Ivan Naluzhnyi <Ivan Naluzhnyi@PC-NALUZHNYI>
2017-10-23 19:52:11 +0200

    Reussi Composant et composant d'un medicament

--- application/views/v_F.php
+++ application/views/v_F.php
@@ -1,4 +1,4 @@
 
 <h1>TEST</h1>
-<a href="http://localhost/PhpProject4/index.php/Ctrl_Medoc/index">Thomas</a>
-<a href="http://localhost/PhpProject4/index.php/Ctrl_Visiteur/index">Sandra</a>
+<a href="<?php echo base_url(); ?>index.php/Ctrl_Medoc/index">Thomas</a>
+<a href="<?php echo base_url(); ?>index.php/Ctrl_Visiteur/index">Sandra</a>
